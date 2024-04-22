42801ba78a2fd8c028173e62ec533270b6175f32
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-04-22 18:48:09 +0200

    Added scheduled every 10 seconds method to delete expired records.

--- src/main/java/pl/pja/s28201/tpo_07/service/FileAndSerializationService.java
+++ src/main/java/pl/pja/s28201/tpo_07/service/FileAndSerializationService.java
@@ -2,8 +2,10 @@ package pl.pja.s28201.tpo_07.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pl.pja.s28201.tpo_07.model.CodeFormat;
+import pl.pja.s28201.tpo_07.repository.InMemoryCodeFormatRepository;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -12,17 +14,18 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
-public class SerializationService {
+public class FileAndSerializationService {
 
     @Value("${serialization.src}")
     private String SERIALIZATION_DIR;
-    private final FileTimeOutService timeOutService;
+    private final InMemoryCodeFormatRepository repository;
+    private final TimeOutService timeOutService;
 
     @Autowired
-    public SerializationService(FileTimeOutService timeOutService) {
+    public FileAndSerializationService(InMemoryCodeFormatRepository repository, TimeOutService timeOutService) {
+        this.repository = repository;
         this.timeOutService = timeOutService;
     }
 
@@ -78,9 +81,26 @@ public class SerializationService {
         return resultMap;
     }
 
-    private void deleteFile(File file) {
+    public void deleteFile(File file) {
         if (file.delete()) {
             System.out.printf("Info:: serialization file with name [%s] has expired and was deleted. %n", file.getName());
+        }
+    }
+
+    @Scheduled(fixedDelay = 10_000)
+    public void deleteOnTimeout() {
+        File[] files = new File(SERIALIZATION_DIR).listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".ser")) {
+                    var toSave = deserialize(file);
+                    if (timeOutService.hasTimeOut(toSave)) {
+                        deleteFile(file);
+                        repository.delete(toSave.getId());
+                    }
+                }
+            }
         }
     }
 }
